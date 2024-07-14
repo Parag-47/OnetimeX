a5e58192b575f423509a971c298888545a19df0d
Parag-47 <144135619+Parag-47@users.noreply.github.com>
2024-07-15 00:19:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,39 @@
-function openPopup() {
-  document.getElementById('demat-registration-popup').style.display = 'block';
-  document.getElementById('background-blur').style.display = 'block';
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+const search_bar = document.querySelector(".search-bar");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+  search_bar.classList.toggle("search-bar-hidden");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+
+
+/* Carousel section*/
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel img');
+const dots = document.querySelectorAll('.dot');
+
+function showNextImage() {
+  images[currentIndex].classList.remove('active');
+  dots[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add('active');
+  dots[currentIndex].classList.add('active');
 }
 
-function closePopup() {
-  document.getElementById('demat-registration-popup').style.display = 'none';
-  document.getElementById('background-blur').style.display = 'none';
-}
+setInterval(showNextImage, 3000); // Change image every 3 seconds
 
-// Close the popup if the user clicks outside of it
-window.onclick = function (event) {
-  var popup = document.getElementById('demat-registration-popup');
-  var blur = document.getElementById('background-blur');
-  if (event.target == blur) {
-    popup.style.display = 'none';
-    blur.style.display = 'none';
-  }
-}
-
-function toggleMenu() {
-  var navLinks = document.querySelector('.nav-links');
-  navLinks.classList.toggle('open');
-}
-
-
-//sliding the text
-document.addEventListener('DOMContentLoaded', function () {
-  const slides = document.querySelectorAll('.sliding-text p');
-  let currentSlide = 0;
-
-  function showNextSlide() {
-    slides[currentSlide].classList.remove('show');
-    slides[currentSlide].classList.add('hide');
-
-    currentSlide = (currentSlide + 1) % slides.length;
-
-    slides[currentSlide].classList.remove('hide');
-    slides[currentSlide].classList.add('show');
-  }
-
-  slides[currentSlide].classList.add('show'); // Show the first slide initially
-
-  setInterval(showNextSlide, 3000); // Change slide every 3 seconds
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    images[currentIndex].classList.remove('active');
+    dots[currentIndex].classList.remove('active');
+    currentIndex = index;
+    images[currentIndex].classList.add('active');
+    dots[currentIndex].classList.add('active');
+  });
 });
-
 
 
